6196280b757fbf2cb77763bed6446ce479e4fddf
Aria Dhanang <ariadhanang@gmail.com>
2023-12-02 21:25:51 +0700

    Add TESTING log

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,3 @@
+console.log("TESTING 1");
+
 if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -1,3 +1,5 @@
+console.log("TESTING 2");
+
 /**
  * Copyright 2018 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
